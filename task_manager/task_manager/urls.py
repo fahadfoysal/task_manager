e026c43a2a061a5851f906159211964012a965a6
Fahad <fahadfoysal16@gmail.com>
2023-12-21 07:57:54 +0600

    Api added

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    # path('api/', include('api.urls')),
+    path('api/', include('api.urls')),
     path('', include('tasks.urls')),
 ]
 
